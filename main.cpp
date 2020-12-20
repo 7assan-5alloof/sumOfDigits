d4f24ca73f3d5c8ca58b9c34588fc3f8c2efb8c2
7assan-5alloof <hassankhalloof20#gmail.com>
2020-12-20 19:55:39 +0400

    Finish program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int sum = 0;
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    int numberWorking = number;
+    for (int i = 10; numberWorking > 0;)
+    {
+        numberWorking %= i;
+        sum += numberWorking;
+        i *= 10;
+    }
+
     return 0;
 }
